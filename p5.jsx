455e537ae6c0aaa4912ff0269bcf4ac5cbaa0f35
Avinash Mallampati <72144928+avinash2906@users.noreply.github.com>
2023-10-02 16:27:30 -0400

    Update p5.jsx

--- p5.jsx
+++ p5.jsx
@@ -1,1 +1,24 @@
-p
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.css';
+import { HashRouter, Route, Link } from "react-router-dom";
+import Header from './components/header/Header';
+import Example from './components/example/Example';
+import States from './components/states/States';
+
+ReactDOM.render(
+    <Header/>,
+    document.getElementById('reactappHeader')
+);
+
+ReactDOM.render(
+    <HashRouter>
+        <div className="container">
+            <div className="menu"><Link to="/examples">Example</Link></div>
+            <div className="menu"><Link to="/states">States</Link></div>
+        </div>
+        <Route path="/states" component={States} />
+        <Route path="/examples" component={Example} />
+    </HashRouter>,
+    document.getElementById('reactapp')
+);
